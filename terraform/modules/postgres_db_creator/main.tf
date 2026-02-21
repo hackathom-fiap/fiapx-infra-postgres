b24a9a13fd92e45d9ffecdcde9892018282533a6
hmouraf <humbertomoura@gmail.com>
2026-02-21 12:36:33 -0300

    new postgres

--- terraform/modules/postgres_db_creator/main.tf
+++ terraform/modules/postgres_db_creator/main.tf
@@ -1,39 +1,21 @@
-variable "host" {
-  description = "The database host address."
-  type        = string
-}
-
-variable "username" {
-  description = "The database username."
-  type        = string
-}
-
-variable "password" {
-  description = "The database password."
-  type        = string
-  sensitive   = true
+terraform {
+  required_providers {
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "~> 1.22.0"
+    }
+  }
 }
 
 variable "db_names" {
-  description = "A map of database names to create."
-  type        = any
-  default     = {}
+  description = "A set of database names to create."
+  type        = set(string)
+  default     = []
 }
 
-provider "postgresql" {
-  alias           = "creator"
-  host            = var.host
-  port            = 5432
-  database        = "postgres"
-  username        = var.username
-  password        = var.password
-  superuser       = false
-  sslmode         = "require"
-  connect_timeout = 15
-}
-
+# This resource will automatically use the 'postgresql' provider
+# configuration passed in from the parent module.
 resource "postgresql_database" "databases" {
-  provider = postgresql.creator
   for_each = var.db_names
 
   name              = each.key
